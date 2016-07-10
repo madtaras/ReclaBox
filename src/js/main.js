d30a3a85a2e515e8d866c5846d67a9f1dd44370a
Taras Mazurkevych <mad.taras@gmail.com>
2016-07-10 04:51:02 +0300

    M

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 (function () {
   require('../../node_modules/materialize-css/dist/js/materialize.js')
+
+  document.querySelector('.sharing-block_favorite-btn')
+    .addEventListener('click', function () {
+      this.classList.toggle('favorited')
+    })
 })()
